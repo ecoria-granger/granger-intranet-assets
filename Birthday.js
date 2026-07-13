b6588b00d0b0be5e2c992b897eac0b691aa5770e
Marcos caula <mcaula@grangernutricion.com>
2026-07-13 09:47:15 -0300

    se agrtegaron fechas de cumpleaños reales

--- Birthday.js
+++ Birthday.js
@@ -17,42 +17,50 @@
  */
 window.CUMPLES = {
     // Enero
-    '01-08': ['Juan Pérez'],
-    '01-22': ['Lucía Fernández'],
+    '01-05': ['Kristal Pereyra Trimmel'],
+    '01-06': ['Rodrigo Sabena'],
+    '01-20': ['Jonatan Rosé'],
 
     // Febrero
-    '02-14': ['Martín Gómez'],
-
+    '02-02': ['Dana Sosa'],
+    '02-03': ['Calvete Gabriel'],
+    '02-22': ['Lobos Jose'],
+    '02-26': ['Ailen Valli'],
     // Marzo
-    '03-03': ['Ana Martínez'],
-    '03-19': ['Carlos López', 'Sofía Ruiz'], // varios el mismo día
+    '03-05': ['Julian Priotti'],
 
     // Abril
     '04-11': ['Laura Díaz'],
 
     // Mayo
-    '05-27': ['Diego Morales'],
-
+    '05-12': ['Gonzalo Moruzze'],
+    '05-19': ['Ignacio Bartizaghi'],
+    '05-20': ['Fernando Imohff'],
+    '05-27': ['Matias Navarro'],
     // Junio
     '06-13': ['Caula Marcos'],
 
     // Julio
     '07-09': ['Coria Elias'],
-
+    '07-26': ['Yaz Sala'],
+    '07-27': ['Sanitago Gutierrez'],
     // Agosto
-    '08-09': ['Camila Torres'],
+    '08-01': ['Gregorio Pontiggia'],
+    '08-14': ['Micaela Villareal'],
+    '08-30': ['Lucia Griguoli'],
 
     // Septiembre
-    '09-21': ['Ricardo Ibáñez'],
-
+    '09-03': ['Maximiliano Longo'],
+    '09-11': ['Maximiliano Flesia'],
     // Octubre
-    '10-04': ['Patricia Núñez'],
+    '10-21': ['Varoli Yamila'],
 
     // Noviembre
-    '11-13': ['Gonzalo Herrera'],
+    '11-08': ['Nerina Aglieri'],
+    '11-10': ['Barrera Thiago'],
 
     // Diciembre
-    '12-02': ['Marina Castro']
+    '12-05': ['Melina Barretto']
 
     // ⬆️ agregá acá los que falten, respetando el formato 'MM-DD': ['Nombre'],
 };
